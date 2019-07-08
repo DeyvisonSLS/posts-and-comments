f4b4ca5bd5f1f567c046146523eb047b373d6445
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-08 16:17:20 -0300

    [First commit] Comment and post class created

---
+++ Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace StringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
